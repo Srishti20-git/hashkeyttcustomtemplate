831dee0a53b64b538c7c9091708b8ca51d9e81ed
Srishti20-git <74393159+Srishti20-git@users.noreply.github.com>
2020-11-17 13:58:47 +0800

    Update template.tsx

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -11,7 +11,18 @@ const containerStyle = css`
   width: 80%;
   text-align: left;
 `;
-//const now = new Date();
+class App extends React.Component {
+  state={
+    curTime : new Date().toLocaleString(),
+  }
+  render(){
+    return (
+      <div className="App">
+        <p>Date : {this.state.curTime}</p>
+      </div>
+    );
+  }
+}
 export const CocTemplate: FunctionComponent<
   TemplateProps<CocTemplateCertificate> & { className?: string }
 > = ({ document, className = "" }) => {
@@ -20,7 +31,7 @@ export const CocTemplate: FunctionComponent<
       <h1>{document.name}</h1>
       <h2>Title: {document.certification.title}</h2>
       <h2>Designation: {document.certification.designation}</h2>
-      <h2>Date: {document.certification.date}</h2>
+      //<h2>Date: {document.certification.date}</h2>
       <h2>InvoiceNo.: {document.certification.invoiceno}</h2>
       <h2>SumRMB: {document.certification.sumRMB}</h2>
       <h2>Signature: {""}</h2>
