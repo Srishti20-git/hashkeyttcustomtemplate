cdb7f69b84c612d63de1a0fdd7d1c03b890d31c5
Srishti20-git <74393159+Srishti20-git@users.noreply.github.com>
2020-11-17 12:37:03 +0800

    Update template.tsx

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -18,38 +18,12 @@ export const CocTemplate: FunctionComponent<
   return (
     <div css={containerStyle} className={className} id="custom-template">
       <h1>{document.name}</h1>
-      <table class="center">
-  <tr>
-    <th>Title</th>
-    <td>New</td>
-  </tr>
-  <tr>
-    <th>Designation</th>
-    <td>Invoice</td>
-  </tr>
-  <tr>
-    <th>Invoice no.</th>
-    <td>10</td>
-  </tr>
-  <tr>
-    <th>Date</th>
-    <td>19/11/2020</td>
-  </tr>
-  <tr>
-    <th>SumRMB</th>
-    <td>22</td>
-  </tr>
-  <tr>
-    <th>Signature</th>
-    <td></td>
-  </tr>
-</table>
-      //<h2>Title: New</h2>
-      //<h2>Designation: {"INVOICE"}</h2>
-      //<h2>InvoiceNo: {"10"}</h2>
-      //<h2>Date: {""}</h2>
-      //<h2>SumRMB: {"222"}</h2>
-      //<h2>Signature: {""}</h2>
+      <h2>Title: New</h2>
+      <h2>Designation: {"INVOICE"}</h2>
+      <h2>InvoiceNo: {"10"}</h2>
+      <h2>Date: {"19/11/2020"}</h2>
+      <h2>SumRMB: {"222"}</h2>
+      <h2>Signature: {""}</h2>
     </div>
   );
 };
