aacf0dcfcb78ac6d0745895a8a14d956a06ed0e7
Srishti20-git <74393159+Srishti20-git@users.noreply.github.com>
2020-11-17 13:46:56 +0800

    Update customTemplateSample.ts

--- src/templates/samples/customTemplateSample.ts
+++ src/templates/samples/customTemplateSample.ts
@@ -32,7 +32,7 @@ export const cocTemplateCertificate: CocTemplateCertificate = {
   },
   certification: {
     title: "New",
-    date: new Date().tolocalestring(),
+    date: new Date().tolocaleString(),
     designation: "Invoice",
     invoiceno: "10",
     sumRMB: "222",
