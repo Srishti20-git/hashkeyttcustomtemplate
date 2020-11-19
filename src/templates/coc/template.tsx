4c9e961c2e59a5f6a2afe0e15cd150c7061bdf80
Srishti20-git <74393159+Srishti20-git@users.noreply.github.com>
2020-11-19 10:54:04 +0800

    Update template.tsx

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -18,11 +18,11 @@ export const CocTemplate: FunctionComponent<
 > = ({ document, className = "" }) => {
   return (
     <div css={containerStyle} className={className} id="custom-template">
-      <h1>CERTIFICATE</h1>
+      <h1>INVOICE DATA</h1>
       <h2>Title: {document.certification.title}</h2>
       <h2>Designation: {document.certification.designation}</h2>
       <h2>Date: {new Date(Date.now()).toLocaleString()}</h2>
-      <h2>InvoiceNo.: 10</h2>
+      <h2>InvoiceNo.: 15</h2>
       <h2>SumRMB: {document.certification.sumRMB}</h2>
       <h2>Signature: {""}</h2>
     </div>
